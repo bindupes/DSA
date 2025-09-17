9baabf3f8a144c4d6c382a210993f625630f56b1
Bindushree <141547551+bindupes@users.noreply.github.com>
2025-09-17 22:06:55 +0530

    Create substring with concatenation.py

---
+++ Math/substring with concatenation.py
@@ -0,0 +1,94 @@
+'''
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+
+Output: [0,9]
+
+Explanation:
+
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+
+Example 2:
+
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+
+Output: []
+
+Explanation:
+
+There is no concatenated substring.
+
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+
+Output: [6,9,12]
+
+Explanation:
+
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"].
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"].
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"].
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+1 <= words.length <= 5000
+1 <= words[i].length <= 30
+s and words[i] consist of lowercase English letters.
+'''
+
+from collections import Counter
+
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        if not s or not words:
+            return []
+
+        word_len = len(words[0])
+        total_len = word_len * len(words)
+        word_count = Counter(words)
+        n = len(s)
+
+        result = []
+
+        # Try each possible starting offset within word length
+        for i in range(word_len):
+            left = i
+            curr_count = Counter()
+            for j in range(i, n - word_len + 1, word_len):
+                word = s[j:j + word_len]
+                if word in word_count:
+                    curr_count[word] += 1
+                    # If too many of this word, move left pointer
+                    while curr_count[word] > word_count[word]:
+                        left_word = s[left:left + word_len]
+                        curr_count[left_word] -= 1
+                        left += word_len
+                    # If window size matches, record index
+                    if j + word_len - left == total_len:
+                        result.append(left)
+                else:
+                    # Reset if word not in words
+                    curr_count.clear()
+                    left = j + word_len
+
+        return result
