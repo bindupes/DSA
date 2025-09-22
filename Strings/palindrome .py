72611d23691ac576fb8bf2ebc10bfaf8beb8cc2d
Bindushree <141547551+bindupes@users.noreply.github.com>
2025-09-22 21:47:09 +0530

    Create palindrome .py

---
+++ Strings/palindrome .py
@@ -0,0 +1,18 @@
+class Three(object):
+    def longest_palindrome(self,s):
+        max_length=0
+        for i in range(1,len(s)-1):
+            left= i-1
+            right=i+1
+            while left>=0 and right<len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+                
+            length = right-left-1
+            if length >max_length:
+                max_length =length
+        
+        return max_length
+                
+sol = Three()
+print(sol.longest_palindrome("babab"))
