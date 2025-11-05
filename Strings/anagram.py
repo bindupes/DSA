c2b3d174d733870a5de5d28645c885b57d80db77
Bindushree <141547551+bindupes@users.noreply.github.com>
2025-11-05 21:44:14 +0530

    Update anagram.py

--- Strings/anagram.py
+++ Strings/anagram.py
@@ -1,14 +1,45 @@
-#Given two strings s1 and s2 consisting of lowercase characters.
+ams(str1,str2#Given two strings s1 and s2 consisting of lowercase characters.
 #The task is to check whether two given strings are an anagram of each other or not. 
 # An anagram of a string is another string that contains the same characters, only the order of characters can be different. 
 # For example, "act" and "tac" are an anagram of each other. Strings s1 and s2 can only contain lowercase alphabets.
 
-
-class Solution:
-    def areAnagrams(self, s1,s2):
-        if len(s1)!=len(s2):
+def anagrams(str1,str2):
+    if len(str1) != len(str2):
+        return False
+        
+    freq={}
+    for char in str1:
+        if char in freq:
+            freq[char]+=1
+        else:
+            freq[char]=1
+        
+    for char in str2:
+        if char not in freq:
             return False
-        return sorted(s1) == sorted(s2)
+        freq[char]-=1
+        if freq[char]<0:
+            return False
+            
+    return True
 
-solution  =Solution()
-print(solution.areAnagrams('geeks','kseeg'))
+
+str1="listen"
+str2="silent"
+print(anagrams(str1,str2))
+
+
+( OR ) 
+
+str1="listen"
+str2="silent"
+
+str1=sorted(str1)
+str2=sorted(str2)
+
+if str1==str2:
+    print("True")
+else:
+    print("False")
+        
+ 
