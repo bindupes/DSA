77b5c190206602ef0035511db8e70d16c4081d76
Bindushree <141547551+bindupes@users.noreply.github.com>
2025-11-05 21:16:40 +0530

    Add script to print the first 15 prime numbers

---
+++ Arrays/first_15_prime.py
@@ -0,0 +1,14 @@
+count =0
+num=2
+
+while count<15:
+  is_prime=True
+  for i in range(2,num):
+    if num%i==0:
+      is_prime=False
+      break
+  if is_prime:
+    print(num,end=" ")
+    count +=1
+  num+=1
+      
