dfcf693a162aa224b314715233060bb0a8024e89
Bindushree <141547551+bindupes@users.noreply.github.com>
2026-01-21 09:23:10 +0530

    Update and rename 9.py to double the array.py

--- Arrays/9.py
+++
@@ -1,5 +0,0 @@
-def double(x):
-    return x*2
-
-print(double(x=[1,2,3,4]))
-
---
+++ Arrays/double the array.py
@@ -0,0 +1,11 @@
+def double(x):
+    for i in range(len(x)):
+        x[i]=x[i]*2 
+    return x
+
+print(double(x=[1,2,3,4])
+
+
+# Output = [2,4,6,8]
+
+
