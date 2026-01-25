74d1e4161c7b6c4749428b762c90ae6fe97e8667
Bindushree <141547551+bindupes@users.noreply.github.com>
2026-01-25 22:19:12 +0530

    Update inversion.py

--- Arrays/inversion.py
+++ Arrays/inversion.py
@@ -2,16 +2,14 @@ class Solution():
     def inversion(self,arr):
         n=len(arr)
         count=0
-        for i in range(0,n-2):
-            for j in range(1,n-1):
-                if arr[i]>arr[j]:
+        for i in range(n):
+            for j in range(i+1,n):
+                if arr[j]>arr[i]:
                     count +=1
-                
-                else:
-                    return 0
         
         return count
 
 solution = Solution()
 print(solution.inversion([2,4,1,3,5]))
+
 
